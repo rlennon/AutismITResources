70d2a8a633fff01762f41a61ccb38fa6dfd9ca85
Oisin O Donnell <L00119483@student.lyit.ie>
2020-12-04 18:20:35 +0000

    php files added so template can be boken down

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="secondary_header footer">
+  <div class="copyright">
+    <p><a href="aboutUs.php">About Us</a></p>
+	Email: autism@123.com<br>
+	Telephone: 087 1234567 <br>
+
+	Covid-19 Helpline: 1800 123 123 <br><br>
+
+  </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,3 @@
+<div class="primary_header">
+  <h1 class="title">Autism IT Resources</h1>
+</div>
