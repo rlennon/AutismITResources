94b1b6d3525beebe79105c5cd0f6b7c01e77658b
PaulDohertyLYIT <45033326+PaulDohertyLYIT@users.noreply.github.com>
2020-12-07 17:50:56 +0000

    Add files via upload

---
+++ phpWebsite/News.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>AutismIt</title>
+<link href="index.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+</head>
+<body>
+<div class="container">
+  <header>
+    <?php include 'header.php';?>
+      <?php include 'navBar.php';?>
+
+  </header>
+  <section>
+    <h2 class="noDisplay">Main Content</h2>
+    <article class="left_article">
+      <h3>News</h3>
+      <p>Here are some useful links that could provide more information about autism</p>
+      <p>These links are to show different layouts we could use in the final product, this is to give the customer a choice on the layout</p>
+      <p><a href="https://autism.ie/news/">Autism.ie</a></p>
+      <p><a href="https://www.spectrumnews.org/news/">spectrumnews.org</a></p>
+      <p><a href="https://www.sciencedaily.com/news/mind_brain/autism/">sciencedaily.com</a></p>
+      <p><a href="https://www.thejournal.ie/autism/news/">thejournal.ie</a></p>
+      <p><a href="https://researchautism.org/news-events/latest-news/">researchautism.org</a></p>
+    </article>
+
+  </section>
+
+  <div class="row blockDisplay">
+    <div class="column_half left_half">
+      <h2 class="column_title"><a href="facebook.com">RESOURCES A-Z</h2>
+    </div>
+    <div class="column_half right_half">
+      <h2 class="column_title"><a href="facebook.com">ARTICLES A-Z</h2>
+    </div>
+  </div>
+  <div class="social">
+
+    <p class="social_icon"><a href="facebook.com"><i class = 'fa fa-facebook' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="twitter.com"><i class = 'fa fa-twitter' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="instagram.com"><i class = 'fa fa-instagram' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="youtube.com"><i class = 'fa fa-youtube' style='font-size:60px;color:#52bad5;'></i></p>
+  </div>
+  <?php include 'footer.php';?>
+</div>
+</body>
+</html>
