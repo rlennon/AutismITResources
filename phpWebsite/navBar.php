987810d5a592642e1645de73e3cf5f90d8da6766
Aaryn MacC <L00088572@student.lyit.ie>
2020-12-07 17:32:50 +0000

    navbar changes, added videoResource page

--- phpWebsite/navBar.php
+++ phpWebsite/navBar.php
@@ -2,9 +2,9 @@
     <ul>
     <li><a href="index.php">HOMEPAGE</a></li>
     <li><a href="image-based-hyperlinks.html">RESOURCE LINKS</a></li>
-    <li>COVID SAFETY</li>
+    <li><a href="videoResources.php">VIDEO RESOURCES</a></li>
     <li>FAQ</li> <!>
-    <li><a href="community.php">COMMUNITY</li>
-    <li>CONTACT US</li>
+    <li><a href="community.php">COMMUNITY</a></li>
+    <li>COVID SAFETY</li>
   </ul>
 </nav>
---
+++ phpWebsite/videoResources.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>AutismIt</title>
+<link href="index.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+</head>
+<body>
+<div class="container">
+  <header>
+    <?php include 'header.php';?>
+      <?php include 'navBar.php';?>
+
+  </header>
+  <section>
+    <h2 class="noDisplay">Main Content</h2>
+    <article class="left_article">
+      <h3>Autism Video Resources</h3>
+
+<h3><p>Below are informative videos in relation to people with autism. They will explain how they can take better care and be supported during Covid-19</p></h3>
+
+<br>
+
+<table> 
+
+<tr>
+
+<td>
+   Click to find out about what Autism is.<br>
+<br>
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/d4G0HTIUBlI">
+</iframe>
+</td>
+
+<td>
+Click to find out how children can be supported during Covid-19<br>
+<br>
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/OUyrLsrz_n0">
+</iframe>
+</td>
+
+<td>
+Click to find out how indivuals with autism can be supported in <br> regards to social distancing
+<br>
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/40ftQ9bla3g">
+</iframe>
+</td>
+
+</tr>
+
+</table>
+<br>
+<a href="News.php">For more information, check out our news resources page</a>
+    </article>
+
+  </section>
+
+  <div class="row blockDisplay">
+    <div class="column_half left_half">
+      <h2 class="column_title"><a href="facebook.com">RESOURCES A-Z</h2>
+    </div>
+    <div class="column_half right_half">
+      <h2 class="column_title"><a href="facebook.com">ARTICLES A-Z</h2>
+    </div>
+  </div>
+  <div class="social">
+
+    <p class="social_icon"><a href="facebook.com"><i class = 'fa fa-facebook' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="twitter.com"><i class = 'fa fa-twitter' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="instagram.com"><i class = 'fa fa-instagram' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="youtube.com"><i class = 'fa fa-youtube' style='font-size:60px;color:#52bad5;'></i></p>
+  </div>
+  <?php include 'footer.php';?>
+</div>
+</body>
+</html>
