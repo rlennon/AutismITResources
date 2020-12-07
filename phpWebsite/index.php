9029df8521500894af0656e37d27835830db898f
Martin Boyle <43784131+QuantumXr2@users.noreply.github.com>
2020-12-07 22:22:58 +0000

    Update to index and added new images

--- phpWebsite/index.php
+++ phpWebsite/index.php
@@ -16,18 +16,59 @@
 
   </header>
   <section>
-    <h2 class="noDisplay">Main Content</h2>
+  <h2 class="noDisplay">Main Content</h2>
     <article class="left_article">
-      <h3>Page heading</h3>
-      <p>text</p>
-      <p>text</p>
-    </article>
+    <h3>About Autism</h3>
+      <p>This a website that contains helpful information about autism support from Letterkenney Institute of Technology. </p>
+      <p>Plrease find in the tabs above different pages with information such as resource links.</p>
+      <p>Autism, or autism spectrum disorder (ASD), refers to a broad range of conditions characterized by challenges with social skills, repetitive behaviors, speech and 
+	  nonverbal communication. We know that there is not one autism but many subtypes, most influenced by a combination of genetic and environmental factors. Because autism 
+	  is a spectrum disorder, each person with autism has a distinct set of strengths and challenges. The ways in which people with autism learn, think and problem-solve can 
+	  range from highly skilled to severely challenged. Some people with ASD may require significant support in their daily lives, while others may need less support and, in 
+	  some cases, live entirely independently. The content above and more can be found at this link: https://www.autismspeaks.org/what-autism
+	  
+    <p>This website aims to assist those with autism and those who care for individuals with autism by providing facilities and resources containg helpfull information. This 
+    has many usefull pages including a news resources, video resouces, covid awarness guidlines and support page where members of the team can be contacted. Feel free to 
+    browse the website and contact us if you have any querries, we are here to help.</p>
+    
+    <p>Also find below four of the most popular articles from the AS I Am website linked in the resources page.</p>
+
+    </p>  
+	</article>
+    <aside class="right_article"> <img src="images/fintownlakesnow.jpg" alt="" width="400" height="200" class="placeholder"> </aside>
+  </section>
+  <div class="row">
+    <div class="columns">
+      <p class="thumbnail_align"> <a href = "https://asiam.ie/about-autism/what-is-autism/" target = "_self">
+      <img src="images/tokyo-yamanote-platforms-e1511453883546.jpg" alt="asiam.ie/about-autism/what-is-autism" class="thumbnail"> </p>
+      <h4>What is Autism</h4>
+      <p>.</p>
+    </div>
+    <div class="columns">
+    <p class="thumbnail_align"> <a href = "https://asiam.ie/about-autism/common-characteristics/" target = "_self">
+      <img src="images/d.jpg" alt="asiam.ie/about-autism/common-characteristics" class="thumbnail"> </p>
+      <h4>Common Characteristics</h4>
+      <p>&nbsp;</p>
+    </div>
+    <div class="columns">
+    <p class="thumbnail_align"> <a href = "https://asiam.ie/about-autism/mythbusters/" target = "_self">
+      <img src="images/WRITING.jpg" alt="asiam.ie/about-autism/mythbusters" class="thumbnail"> </p>
+      <h4>Mythbusters</h4>
+      <p>&nbsp;</p>
+    </div>
+    <div class="columns">
+    <p class="thumbnail_align"> <a href = "https://asiam.ie/about-autism/autism-spectrum-related-conditions/" target = "_self">
+      <img src="images/umbrellas.jpg" alt="asiam.ie/about-autism/autism-spectrum-related-conditions" class="thumbnail"> </p>
+      <h4>Related Conditions</h4>
+      <p>&nbsp;</p>
+    </div>
+  </div>
 
   </section>
 
   <div class="row blockDisplay">
     <div class="column_half left_half">
-      <h2 class="column_title"><a href="image-based-hyperlinks.php">RESOURCES A-Z</h2>
+      <h2 class="column_title"><a href="image-based-hyperlinks.php">RESOURCES </h2>
     </div>
     <div class="column_half right_half">
       <h2 class="column_title"><a href="News.php">ARTICLES A-Z</h2>
