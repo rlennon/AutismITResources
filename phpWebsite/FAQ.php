ff1442cab021239c0370e291cd152dcfaca75467
Sean Gallagher <L00137495@student.lyit.ie>
2020-12-07 18:24:42 +0000

    Updated FAQ page

---
+++ phpWebsite/FAQ.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>AutismIt</title>
+<link href="index.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+</head>
+<body>
+<div class="container">
+  <header>
+    <?php include 'header.php';?>
+      <?php include 'navBar.php';?>
+
+  </header>
+  <section>
+    <h2 class="noDisplay">Main Content</h2>
+    <article class="left_article">
+      <h1>Frequently asked questions (FAQ)</h1>
+      <br>
+      <p><h2>Symptoms of autism</h2></p>
+      <p>Autism, like many other neurodivergent conditions, is an invisible disability which exists on a spectrum. <br>
+      Therefore, autistic traits are not universal and are often specific to the person.
+      <a href="https://asiam.ie/about-autism/common-characteristics/"> Click here</a> for an in <br>depth description of autistic traits.</p>
+      <p><h2>How to access services and support</h2></p>
+      <p>To access services and supports that may have
+      been recommended for a child in relation to their education <br>
+       see the<a href="https://ncse.ie/for-schools"> National Council for Special Education</a> website.</p>
+      <br>
+  </section>
+
+  <div class="row blockDisplay">
+    <div class="column_half left_half">
+      <h2 class="column_title"><a href="facebook.com">RESOURCES A-Z</h2>
+    </div>
+    <div class="column_half right_half">
+      <h2 class="column_title"><a href="facebook.com">ARTICLES A-Z</h2>
+    </div>
+  </div>
+  <div class="social">
+
+    <p class="social_icon"><a href="facebook.com"><i class = 'fa fa-facebook' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="twitter.com"><i class = 'fa fa-twitter' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="instagram.com"><i class = 'fa fa-instagram' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="youtube.com"><i class = 'fa fa-youtube' style='font-size:60px;color:#52bad5;'></i></p>
+  </div>
+  <?php include 'footer.php';?>
+</div>
+</body>
+</html>
