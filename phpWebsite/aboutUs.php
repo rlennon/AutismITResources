b5867345e19abdd236b239378685b464916c15de
Oisin O Donnell <L00119483@student.lyit.ie>
2020-12-08 01:00:27 +0000

    aboutus page added to folder along with images for community and aboutus page

---
+++ phpWebsite/aboutUs.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>AutismIt</title>
+<link href="index.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+</head>
+<body>
+<div class="container">
+  <header>
+    <?php include 'header.php';?>
+      <?php include 'navBar.php';?>
+
+  </header>
+  <section>
+  <h2 class="noDisplay">Main Content</h2>
+    <article class="left_article">
+    <h3>About Us</h3>
+      <p>This website aims to bring all the revelant resources for autistic individuals in one stop place so they dont have to. This website will be displayed within a colour scheme that is autism friendly.
+
+We here aim to assist autistic individuals in any way possible. The current COVID19 global pandemic has affected almost everyone thorughout the globe.
+
+Our aim is to bring a some needed assistance to autistic individuals because like a lot of other people this pandemic has effected the way we learn.</p>
+
+    </p>
+	</article>
+    <aside class="right_article"> <img src="images/autism.png" alt="" width="400" height="200" class="placeholder"> </aside>
+  </section>
+  <div class="row">
+    <div class="columns">
+      <p class="thumbnail_align"> <a href = "community.php" target = "_self">
+      <img src="images/community.png" alt="asiam.ie/about-autism/what-is-autism" class="thumbnail"> </p>
+      <h4>Community</h4>
+      <p>Join Our Community and talk about your favourate resources with others!!</p>
+    </div>
+  </div>
+
+  </section>
+
+  <div class="row blockDisplay">
+    <div class="column_half left_half">
+      <h2 class="column_title"><a href="image-based-hyperlinks.php">RESOURCES </h2>
+    </div>
+    <div class="column_half right_half">
+      <h2 class="column_title"><a href="News.php">ARTICLES</h2>
+    </div>
+  </div>
+  <div class="social">
+
+    <p class="social_icon"><a href="https://www.facebook.com"><i class = 'fa fa-facebook' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="https://www.twitter.com"><i class = 'fa fa-twitter' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="https://www.instagram.com"><i class = 'fa fa-instagram' style='font-size:60px;color:#52bad5;'></i></p>
+    <p class="social_icon"><a href="https://www.youtube.com"><i class = 'fa fa-youtube' style='font-size:60px;color:#52bad5;'></i></p>
+  </div>
+  <?php include 'footer.php';?>
+</div>
+</body>
+</html>
