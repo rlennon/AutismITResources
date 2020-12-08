deeedfbf611dd7fc7a02c2051f64749953f45d42
L00142041 <L00142041@student.lyit.ie>
2020-12-08 09:23:37 +0000

    Changed ATT to Tech in Navbar.

--- phpWebsite/navBar.php
+++ phpWebsite/navBar.php
@@ -1,10 +1,10 @@
 <nav class="secondary_header" id="menu">
     <ul>
-    <li><a href="index.php">HOMEPAGE</a></li>
-    <li><a href="videoResources.php">VIDEO RESOURCES</a></li>
-    <li><a href="FAQ.php">FAQ</a></li>
-    <li><a href="staySecure.php">STAYING SAFE</a></li>
-    <li><a href="covidAdvice.php">COVID SAFETY</a></li>
-    <li><a href="assistiveTechPage.php">ATT</a></li>
-  </ul>
+        <li><a href="index.php">HOMEPAGE</a></li>
+        <li><a href="videoResources.php">VIDEO RESOURCES</a></li>
+        <li><a href="FAQ.php">FAQ</a></li>
+        <li><a href="staySecure.php">STAYING SAFE</a></li>
+        <li><a href="covidAdvice.php">COVID SAFETY</a></li>
+        <li><a href="assistiveTechPage.php">TECH</a></li>
+    </ul>
 </nav>
