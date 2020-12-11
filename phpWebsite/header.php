6c70d25a3d6c5bf8bdba3afc729be4eefba7ea53
Aaryn MacC <L00088572@student.lyit.ie>
2020-12-11 21:19:01 +0000

    added userDetails page and created a link to it on the header

--- phpWebsite/header.php
+++ phpWebsite/header.php
@@ -1,3 +1,4 @@
 <div class="primary_header">
-  <h1 class="title">Autism IT Resources</h1> <a href = "login.php">Login</a>
+  <h1 class="title">Autism IT Resources</h1> <a href = "login.php">Login</a> &nbsp;&nbsp;&nbsp;&nbsp;
+<a href = "userDetails.php">View Account Details</a>
 </div>
