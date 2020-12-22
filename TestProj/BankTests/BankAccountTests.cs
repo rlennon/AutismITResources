4888dfed9a3f16782d995fee2556a2674c46ef4d
Oisin Costello <l00143552@student.lyit.ie>
2020-12-22 19:28:56 +0000

    Created a unit test in C# for a debit method in a bank account class

---
+++ TestProj/BankTests/BankAccountTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double startBalance = 1100;
+            double debitAmount = 100;
+            double expected = 1000;
+            BankAccount account = new BankAccount("John Doe", startBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
---
+++ TestProj/TestProj/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BankAccountNS
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double accbalance;
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            accbalance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return accbalance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > accbalance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            accbalance -= amount;
+        }
+
+        
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. John Doe", 2000);
+
+            
+            ba.Debit(100);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
